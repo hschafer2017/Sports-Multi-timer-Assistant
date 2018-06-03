3bc2dd4a2421b83e6b539691c3732110c667161e
Haley Schafer <hschafer@umich.edu>
2018-06-03 09:32:46 +0000

    Page Render Functions, Login, and User Greeting

--- timer.py
+++ timer.py
@@ -1,13 +1,24 @@
 import os
 from flask import Flask, redirect, render_template, request
 
-
 app = Flask(__name__)
 
+@app.route('/')
+def get_homepage():
+   return render_template('index.html')
+   
+@app.route('/login', methods=['POST'])
+def login(): 
+    team = request.form['team']
+    username = request.form['username']
+    return redirect("/" + team + "/" + username) 
+# Pass two arguments through redirect without having to make an extra page 
 
-
+@app.route('/<team>/<username>')
+def get_user_page(team, username):
+    return render_template('timer_page.html', username = username, team = team)
 
 
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
+    app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug = True)
