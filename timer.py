1f9dd84096bb6e34498ed321e99444c1cb25375d
Haley Schafer <hschafer@umich.edu>
2018-06-06 08:55:03 +0000

    Ability to choose event, heat, and lanes with a redirect

--- timer.py
+++ timer.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, redirect, render_template, request
 
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,13 +13,23 @@ def login():
     sport = request.form['sport']
     team = request.form['team']
     username = request.form['username']
-    return redirect("/" + sport + "/" + team + "/" + username) 
-# Pass two arguments through redirect without having to make an extra page 
+    meet = request.form['meet']
+    return redirect("/" + sport + "/" + team + "/" + username + "/" + meet) 
 
-@app.route('/<sport>/<team>/<username>')
-def get_user_page(sport, team, username):
-    return render_template('timer_page.html', username = username, team = team, sport = sport)
+@app.route('/<sport>/<team>/<username>/<meet>')
+def get_user_page(sport, team, username, meet):
+    return render_template('timer_page.html', username = username, team = team, sport = sport, meet = meet)
 
+@app.route('/<sport>/<team>/<username>/<meet>/new', methods = ['POST'])
+def timer_setup(sport, team, username, meet):
+    event = request.form['Event']
+    heat = request.form['Heat']
+    lane_count = request.form['Lanes']
+    return redirect("/" + sport + "/" + team + "/" + username + "/" + meet + '/' + event + "/" + heat + "/" + lane_count)
+
+@app.route('/<sport>/<team>/<username>/<meet>/<event>/<heat>/<lane_count>')
+def timer_set(sport, team, username, meet, event, heat, lane_count):
+    return render_template('timer_page.html', username = username, team = team, sport = sport, meet = meet, event = event, heat = heat, lane_count = lane_count)
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug = True)
